6ef1c6f901f2a0fd13a1261a87ad9bd79fdd65b2
Pablo <augustopablo99@gmail.com>
2025-11-12 21:21:15 -0300

    Resolve conflito do stash após pull

--- minhasenha/asgi.py
+++ minhasenha/asgi.py
@@ -3,12 +3,12 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing # Importa o arquivo de rotas do seu app 'core'
-
+ 
 # CORRIGIDO: A variável de ambiente aponta para o nome do projeto 'minhasenha'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minhasenha.settings') 
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'minhasenha.settings')
+ 
 django_asgi_app = get_asgi_application()
-
+ 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
